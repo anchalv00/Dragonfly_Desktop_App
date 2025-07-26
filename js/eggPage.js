6ca9961aa4e7cc79bf1ad68f27f4d5e555ac8a71
anchalv00 <anchal.verma.av16@gmail.com>
2025-07-25 19:15:33 -0700

    Updated imaged path

--- js/eggPage.js
+++ js/eggPage.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const eggBtn = document.getElementById('eggImg');
 
-  let eggImagesList = ["assets/images/red-1.png", "assets/images/blue-1.png", "assets/images/green-1.png"]
+  let eggImagesList = ["../assets/images/red-1.png", "../assets/images/blue-1.png", "../assets/images/green-1.png"]
 
 
   let randomIndex = Math.floor(Math.random() * eggImagesList.length);
@@ -20,15 +20,15 @@ window.addEventListener('DOMContentLoaded', () => {
     clickCount++;
     
     if (randomIndex == 0) {
-        eggBtn.src = `assets/images/red-${clickCount}.png`;
+        eggBtn.src = `../assets/images/red-${clickCount}.png`;
     }else if (randomIndex == 1) {
-        eggBtn.src = `assets/images/blue-${clickCount}.png`;
+        eggBtn.src = `../assets/images/blue-${clickCount}.png`;
     }else if (randomIndex == 2) {
-        eggBtn.src = `assets/images/green-${clickCount}.png`;
+        eggBtn.src = `../assets/images/green-${clickCount}.png`;
     }
 
 
-    if (clickCount > 6) {
+    if (clickCount > 5) {
         window.api.goToDragonPage();
     }
   });
