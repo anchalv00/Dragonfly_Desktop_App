f18d0dd27347145de5a89a2b3f76d805a0732f73
anchalv00 <anchal.verma.av16@gmail.com>
2025-07-26 21:40:37 -0700

    Fixed level num

---
+++ DragonflyApp-win32-x64/resources/app/js/metricsPage.js
@@ -0,0 +1,58 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+ const dragonImg = document.getElementById('dragonImg');
+ const dragonName = document.getElementById('dragonName');
+ const dragonOrigin = document.getElementById('dragonOrigin');  
+ const dragonType = document.getElementById('dragonType');
+ const exitBtn = document.getElementById('exitBtn');
+
+ dragonImg.style.display = "block";
+
+
+let redDragonMetrics = ["Blaze", "Volcanic Island", "Fire"];
+let blueDragonMetrics = ["Bluey", "Icy Atlantic", "Water"];
+let greenDragonMetrics = ["Brownie", "West Jungle", "Earth"];
+
+
+window.api.getDragon().then((chosenDragonImg) => {
+    if (chosenDragonImg) {
+      console.log("Chosen Dragon Image:", chosenDragonImg);
+      document.getElementById('dragonImg').src = chosenDragonImg;
+    }
+});
+
+window.api.getRandomInt().then((randomInt) => {
+  console.log("Random Int:", randomInt);
+  const randomIntConverted = Number(randomInt);
+
+  if (randomIntConverted >= 0 && randomIntConverted < 3) {
+    console.log("Chosen Random Int:", randomIntConverted);
+    if (randomIntConverted == 0) {
+      dragonName.textContent = redDragonMetrics[0];
+      dragonOrigin.textContent = redDragonMetrics[1];
+      dragonType.textContent = redDragonMetrics[2];
+    } else if (randomIntConverted == 1) {
+      dragonName.textContent = blueDragonMetrics[0];
+      dragonOrigin.textContent = blueDragonMetrics[1];
+      dragonType.textContent = blueDragonMetrics[2];
+    } else if (randomIntConverted == 2) {
+      dragonName.textContent = greenDragonMetrics[0];
+      dragonOrigin.textContent = greenDragonMetrics[1];
+      dragonType.textContent = greenDragonMetrics[2];
+    }
+    dragonName.classList.remove('hidden');
+    dragonOrigin.classList.remove('hidden');
+    dragonType.classList.remove('hidden');
+  }
+    
+});
+
+exitBtn.addEventListener('click', () => {
+  console.log("Exit Button clicked");
+  buttonAudio.play();
+  setTimeout(() => {
+    window.api.goToDragonPage();
+  }, 1500); 
+});
+
+});
---
+++ DragonflyApp-win32-x64/resources/app/preload.js
@@ -0,0 +1,13 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("api", {
+    goToEggPage: () => ipcRenderer.send("load-egg-page"),
+    goToDragonPage: () => ipcRenderer.send("load-dragon-page"),
+    setDragon: (data) => ipcRenderer.send("set-dragon", data),
+    getDragon: () => ipcRenderer.invoke('get-dragon'),
+    setRandomInt: (randomInt) => ipcRenderer.send("set-random-int", randomInt),
+    getRandomInt: () => ipcRenderer.invoke("get-random-int"),
+    goToMetricsPage: () => ipcRenderer.send("load-metrics-page"),
+    saveProgress: (progressData) => ipcRenderer.send('save-progress', progressData),
+    getProgress: () => ipcRenderer.invoke('get-progress')
+});
